690af1d6e920ef545583668f807f1ac5421ba92d
xbrtoufik <xbrtoufik@gmail.com>
2025-10-13 01:00:25 +0600

    router1

--- single page/src/main.jsx
+++ single page/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter,Route,Routes } from "react-router";
+import Contact from './pages/Contact.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+  </BrowserRouter>,
 )
